c64f14e29e60a84c611aafbb2d74716b7758d761
YoussefPasha <jojoyossefbasha010@gmail.com>
2020-11-14 22:36:18 +0200

    done with add to cart functionality and styling

--- client/src/App.js
+++ client/src/App.js
@@ -4,19 +4,29 @@ import { Route } from "react-router-dom/cjs/react-router-dom.min";
 import ProductScreen from "./screens/ProductScreen";
 import HomeScreen from "./screens/HomeScreen";
 import CartScreen from "./screens/CartScreen";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 function App() {
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
   return (
     <BrowserRouter>
       <div className="grid-container">
         <header className="row">
           <div>
-            <a className="brand" href="/">
+            <Link className="brand" to="/">
               Amazon
-            </a>
+            </Link>
           </div>
           <div>
-            <a href="/">Cart</a>
-            <a href="/">Sign In</a>
+            <Link to="/">
+              Cart
+              {cartItems.length > 0 && (
+                <span className="badge">{cartItems.length}</span>
+              )}
+            </Link>
+            <Link to="/">Sign In</Link>
           </div>
         </header>
         <main>
--- client/src/screens/CartScreen.js
+++ client/src/screens/CartScreen.js
@@ -1,8 +1,15 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import { addToCart } from "../actions/cartActions";
+import { useDispatch } from "react-redux";
 export default function CartScreen({ match, location }) {
   const productId = match.params.id;
   const qty = location.search ? Number(location.search.split("=")[1]) : 1;
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty));
+    }
+  }, [dispatch, productId, qty]);
   return (
     <div>
       <h1>Cart Screen</h1>
