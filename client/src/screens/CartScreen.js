6299e72b30270572838ebde4a2cb9b24eb37f6d7
Youssef Pasha <jojoyossefbasha010@gmail.com>
2020-11-19 00:33:42 +0200

    remove from cart

--- client/src/screens/CartScreen.js
+++ client/src/screens/CartScreen.js
@@ -1,5 +1,5 @@
 import React, { Fragment, useEffect } from "react";
-import { addToCart } from "../actions/cartActions";
+import { addToCart, removeFromCart } from "../actions/cartActions";
 import { useDispatch, useSelector } from "react-redux";
 import MessageBox from "../components/MessageBox";
 import { Link } from "react-router-dom";
@@ -15,7 +15,7 @@ export default function CartScreen({ match, location, history }) {
     }
   }, [dispatch, productId, qty]);
   const removeFromCartHandler = (id) => {
-    //delete from Cart
+    dispatch(removeFromCart(id));
   };
   const checkoutHandler = () => {
     history.push("/signin?redirect=shipping");
