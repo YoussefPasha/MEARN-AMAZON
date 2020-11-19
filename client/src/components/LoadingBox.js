c8876ba8a24a9340b8e12f2e939e1f9f66771ff4
Youssef Pasha <jojoyossefbasha010@gmail.com>
2020-11-19 04:25:10 +0200

    productModel and call it from mongodb

--- client/src/components/LoadingBox.js
+++ client/src/components/LoadingBox.js
@@ -1,9 +1,5 @@
 import React from "react";
 
 export default function LoadingBox() {
-  return (
-    <div className="loading">
-      <i className="fa fa-spinner fa-spin"></i> Loading...
-    </div>
-  );
+  return <div class="Loader">Loading...</div>;
 }
