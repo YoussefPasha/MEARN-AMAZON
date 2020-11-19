38028e81c3ab6d2e21b68a8ae45f4e5560a8f30d
Youssef Pasha <jojoyossefbasha010@gmail.com>
2020-11-19 02:28:09 +0200

    create user Schema and db

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,20 @@
+import bcrypt from "bcryptjs";
+
 const data = {
+  users: [
+    {
+      name: "Youssef",
+      email: "admin@example.com",
+      password: bcrypt.hashSync("1234", 8),
+      isAdmin: true,
+    },
+    {
+      name: "JohnDoe",
+      email: "JohnDoe@JohnDoe.io",
+      password: bcrypt.hashSync("1234", 8),
+      isAdmin: false,
+    },
+  ],
   products: [
     {
       _id: "1",
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,20 @@
 import express from "express";
 import data from "./data.js";
-
+import mongoose from "mongoose";
+import userRouter from "./routers/userRouter.js";
 const app = express();
+
+mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/MERN_AMAZON", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
 app.get("/", (req, res) => {
   res.send("Server is Ready");
 });
+
+app.use("/api/users", userRouter);
 
 app.get("/api/products/:id", (req, res) => {
   const product = data.products.find((x) => x._id === req.params.id);
@@ -17,6 +27,10 @@ app.get("/api/products/:id", (req, res) => {
 
 app.get("/api/products", (req, res) => {
   res.send(data.products);
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
 });
 
 const port = process.env.PORT || 5000;
