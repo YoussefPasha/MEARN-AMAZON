5db7df45f42922af2476682de2b7353f0a5e9c01
YoussefPasha <jojoyossefbasha010@gmail.com>
2020-12-01 06:08:32 +0200

    reducer and back end for create product

--- client/src/reducers/productReducers.js
+++ client/src/reducers/productReducers.js
@@ -6,6 +6,10 @@ import {
   PRODUCT_LIST_FAIL,
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
+  PRODUCT_CREATE_REQUEST,
+  PRODUCT_CREATE_SUCCESS,
+  PRODUCT_CREATE_FAIL,
+  PRODUCT_CREATE_RESET,
 } from "../constants/productConstants";
 
 export const productListReducer = (
@@ -49,6 +53,20 @@ export const productDetailsReducer = (
         loading: false,
         error: action.payload,
       };
+    default:
+      return state;
+  }
+};
+export const productCreateReducer = (state = {}, action) => {
+  switch (action.type) {
+    case PRODUCT_CREATE_REQUEST:
+      return { loading: true };
+    case PRODUCT_CREATE_SUCCESS:
+      return { loading: false, success: true, product: action.payload };
+    case PRODUCT_CREATE_FAIL:
+      return { loading: false, error: action.payload };
+    case PRODUCT_CREATE_RESET:
+      return {};
     default:
       return state;
   }
--- client/src/store.js
+++ client/src/store.js
@@ -3,6 +3,7 @@ import thunk from "redux-thunk";
 import {
   productDetailsReducer,
   productListReducer,
+  productCreateReducer,
 } from "./reducers/productReducers";
 import { cartReducer } from "./reducers/cartReducers";
 import {
@@ -45,6 +46,7 @@ const reducer = combineReducers({
   orderMineList: orderMineListReducer,
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
+  productCreate: productCreateReducer,
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
