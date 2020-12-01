df0d49865eb2b2c801af0fc785b4bb283f516a78
YoussefPasha <jojoyossefbasha010@gmail.com>
2020-12-01 06:30:32 +0200

    create redux for Update product

--- client/src/store.js
+++ client/src/store.js
@@ -4,6 +4,7 @@ import {
   productDetailsReducer,
   productListReducer,
   productCreateReducer,
+  productUpdateReducer,
 } from "./reducers/productReducers";
 import { cartReducer } from "./reducers/cartReducers";
 import {
@@ -47,6 +48,7 @@ const reducer = combineReducers({
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
   productCreate: productCreateReducer,
+  productUpdate: productUpdateReducer,
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
