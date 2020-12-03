2c53d8f3754ef6c3c5dbd8d0796d7c5260b794e6
Youssef Pasha <jojoyossefbasha010@gmail.com>
2020-12-03 03:20:23 +0200

    create seller as user view

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -11,6 +11,7 @@ const productSchema = mongoose.Schema(
     countInStock: { type: Number, required: true },
     rating: { type: Number, required: true },
     numReviews: { type: Number, required: true },
+    seller: { type: mongoose.Schema.Types.ObjectID, ref: "User" },
   },
   {
     timestamps: true,
